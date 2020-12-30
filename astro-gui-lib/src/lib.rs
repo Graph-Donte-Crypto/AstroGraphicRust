7c48430cc8ba4e260e742ae89c1c41f07f4148fd
Graph-Donte-Crypto <Dimasik841@gmail.com>
2020-12-30 05:24:51 +0300

    Getting radius vector via matrix works

--- astro-gui-lib/src/lib.rs
+++ astro-gui-lib/src/lib.rs
@@ -1,11 +1,15 @@
 //extern crate nalgebra as na;
-
+#[macro_use(lazy_static)]
+extern crate lazy_static;
 use kiss3d::nalgebra as na;
 
 use na::{Vector3, UnitQuaternion, Translation3, Point3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 use kiss3d::event::{Key, MouseButton};
+
+//pub use kiss3d::nalgebra;
+pub use kiss3d;
 
 pub struct Orbit {
     a: f32,
@@ -21,7 +25,7 @@ pub struct Orbit {
     period_value: f32,  // orbital period
     e_root: f32,	     // sqrt(1 - e^2)
     a_e_root: f32,	     // a * e_root
-    orb_to_ecl: [f32; 6]
+    orb_to_ecl: [f32; 6] 
 }
 
 impl Orbit {
@@ -31,11 +35,13 @@ impl Orbit {
         let mean_motion = speed_root / a;
         let e_root = (1.0 - e * e).sqrt();
         let i_rad = i.to_radians();
+        let om_big = omega_big.to_radians();
+        let om_sma = omega_big.to_radians();
         let mut orbit = Orbit {
             a, 
             e, 
-            omega_big: omega_big.to_radians(), 
-            omega_small: omega_small.to_radians(), 
+            omega_big: om_big, 
+            omega_small: om_sma, 
             i: i_rad,
             mu,
             points: Vec::<Point3<f32>>::new(),
@@ -46,12 +52,12 @@ impl Orbit {
             e_root,
             a_e_root: a * e_root,
             orb_to_ecl : [
-                omega_big.cos() * omega_small.cos() - omega_big.sin() * omega_small.sin() * i_rad.cos(),
-                -omega_big.cos() * omega_small.sin() - omega_big.sin() * omega_small.cos() * i_rad.cos(),
-                omega_big.sin() * omega_small.cos() + omega_big.cos() * omega_small.sin() * i_rad.cos(),
-                -omega_big.sin() * omega_small.sin() - omega_big.cos() * omega_small.cos() * i_rad.cos(),
-                omega_small.sin() * i_rad.sin(),
-                omega_small.cos() * i_rad.sin()
+                 om_big.cos() * om_sma.cos() - om_big.sin() * om_sma.sin() * i_rad.cos(),
+                -om_big.cos() * om_sma.sin() - om_big.sin() * om_sma.cos() * i_rad.cos(),
+                 om_big.sin() * om_sma.cos() + om_big.cos() * om_sma.sin() * i_rad.cos(),
+                -om_big.sin() * om_sma.sin() + om_big.cos() * om_sma.cos() * i_rad.cos(),
+                om_sma.sin() * i_rad.sin(),
+                om_sma.cos() * i_rad.sin()
             ]
         };
         orbit.generate_orbit_points(points_count);
@@ -62,11 +68,24 @@ impl Orbit {
         return &self.points;
     }
 
-    /*
+    
     fn get_radius(&self, nu: f32) -> f32 {
         return (self.a * (1.0 - self.e * self.e)) / (1.0 + self.e * nu.cos());
     }
-    */
+   
+
+    pub fn get_anti_posos(&self, nu: f32) -> Vector3<f32>{
+        let W = self.omega_big;
+        let w = self.omega_small;
+        let i = self.i;
+        let v = w + nu;
+
+        let r: f32 = self.get_radius(nu);
+        let x = r * (W.cos() * v.cos() - W.sin() * v.sin() * i.cos());
+        let y = r * (W.sin() * v.cos() + W.cos() * v.sin() * i.cos());
+        let z = r * (v.sin() * i.sin());
+        return Vector3::new(x, y, z);
+    } 
 
     //nu in [0; 2*PI]
     fn get_radius_vector(&self, nu: f32) -> Vector3<f32>{
@@ -84,13 +103,17 @@ impl Orbit {
         return Vector3::new(x, y, z);
         */
 
+        
         let e = self.e;
         let a = self.a;
         let e_root = self.e_root;
         let ote = &self.orb_to_ecl;
 
-        let EA_cos = (e + nu.cos()) * (1.0 + e * nu.cos());
-        let EA_sin = e_root * nu.sin() / (e + nu.cos());
+        let EA_cos = (e + nu.cos()) / (1.0 + e * nu.cos());
+        let EA_sin = e_root * nu.sin() / (1.0 + e * nu.cos());
+        
+        println!("EA_cos {}", EA_cos);
+        println!("EA_sin {}", EA_sin);
 
         let r_x = a * (EA_cos - e);
         let r_y = e_root * a * EA_sin;
@@ -100,7 +123,7 @@ impl Orbit {
             ote[2] * r_x + ote[3] * r_y,  
             ote[4] * r_x + ote[5] * r_y  
         );
-
+        
     } 
 
     pub fn get_position_and_velocity(&self, nu: f32) -> (Point3<f32>, Vector3<f32>) {
@@ -137,13 +160,37 @@ impl Orbit {
 
     //pub fn get_point_and_speed(&self, nu: f32)
 
+    pub fn generate_anti_pososat(&self, count: usize) -> Vec<Point3<f32>>{
+        use core::f32::consts::TAU;
+        let mut vec = Vec::<Point3<f32>>::new();
+        let mut nu: f32 = 0.0;
+
+        for _ in 0..count {
+            let point = Point3::from(self.get_anti_posos(nu));
+            println!("nu = {}", nu.to_degrees());
+            println!("    X = {}", point[0]);
+            println!("    Y = {}", point[1]);
+            println!("    Z = {}", point[2]);
+            println!();
+            vec.push(point);
+            nu += TAU / (count as f32);
+        }    
+        return vec;
+    }
+
     fn generate_orbit_points(&mut self, count: usize) {
         use core::f32::consts::TAU;
 
         let mut nu: f32 = 0.0;
 
         for i in 0..count {
-            self.points.push(Point3::from(self.get_radius_vector(nu)));
+            let point = Point3::from(self.get_radius_vector(nu));
+            println!("nu = {}", nu.to_degrees());
+            println!("    X = {}", point[0]);
+            println!("    Y = {}", point[1]);
+            println!("    Z = {}", point[2]);
+            println!();
+            self.points.push(point);
             nu += TAU / (count as f32);
         }
     }
@@ -151,6 +198,7 @@ impl Orbit {
 
 pub struct SpaceBody {
     orbit: Orbit,
+    r: f32,
     nu0: f32,
     nu: f32,
     position : Point3<f32>,
@@ -159,89 +207,59 @@ pub struct SpaceBody {
 
 impl SpaceBody {
     
-    pub fn move_to_angle(&mut self, nu: f32) {
+    pub fn new(orbit: Orbit, r:f32, nu0 : f32, nu: f32) -> SpaceBody { 
+        let (position, velocity) = orbit.get_position_and_velocity(nu);
+
+        return SpaceBody {
+            orbit,
+            r,
+            nu0,
+            nu,
+            position,
+            velocity
+        };
+    }
+
+
+    pub fn move_to_true_anomaly(&mut self, nu: f32) {
         self.nu = nu;
-        let result = &self.orbit.get_position_and_velocity(nu);
-        self.position = result.0;
-        self.velocity = result.1;
+        let (position, velocity) = self.orbit.get_position_and_velocity(nu);
+        self.position = position;
+        self.velocity = velocity;
+    }
+
+    pub fn get_position(&self) -> &Point3<f32> {
+        return &self.position;
+    }
+
+    pub fn get_position_as_translation(&self) -> Translation3<f32> {
+        return Translation3::<f32>::from(self.position.coords);
+    }
+
+    pub fn get_velocity(&self) -> &Vector3<f32> {
+        return &self.velocity;
+    } 
+
+    pub fn get_true_anomaly(&self) -> f32 {
+        return self.nu;
     }
 }
 
-fn main() {
-    let mut window = Window::new("Astro Graphic Rust");
-    //let mut c      = window.add_cube(1.0, 1.0, 1.0);
 
-    let mut sphere = window.add_sphere(1.0);
-    //let planet = window.add_sphere(0.2);
+lazy_static! {
 
-    sphere.set_color(1.0, 1.0, 0.0);
+    static ref MOHO_EXAMPLE: SpaceBody = SpaceBody::new(
+        Orbit::new(5.263138, 0.2, 7.0, 15.0, 70.0, 1172.3328, 500),
+        2.5,
+        180_f32.to_radians(),
+        0.0
+    );
 
-    window.set_light(Light::StickToCamera);
+}
 
-
-    //let tra = Translation3::new(1.0, 2.0, 2.0);
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
-    
-    //let PI = 3.141592656;
-    //let mut nu : f32 = 0.0;
-
-
-    let orbit = Orbit::new(5.263138, 0.2, 7.0, 15.0, 70.0, 1172.3328, 500);
-
-
-    let points_count : usize = 3 * 360;
-    //let vec = orbit.get_orbit_points(points_count as i32);
-
-    //planet.set_local_translation(Translation3::new(1.0, 0.0, 0.0));
-    
-    //sphere.prepend_to_local_translation(tra);
-    //sphere.set_local_translation(tra);
-
-    window.set_line_width(1.0);
-
-    let mut index : usize = 0;
-
-    fn point_to_vector(point: &Point3<f32>) -> Vector3<f32> {
-        return Vector3::new(point.x, point.y, point.z);
+pub fn draw_lines(window: &mut kiss3d::window::Window, points: &Vec<Point3<f32>>, color: &Point3<f32>) {
+    for i in 0..points.len()-1 {
+        window.draw_line(&points[i], &points[i + 1], &color);
     }
-
-    let box_size : f32 = 500.0;
-
-    let axis_0 = Point3::new(-box_size, -box_size, -box_size);
-    let axis_x = Point3::new(box_size, -box_size, -box_size);
-    let axis_y = Point3::new(-box_size, box_size, -box_size);
-    let axis_z = Point3::new(-box_size, -box_size, box_size);
-
-    let mut camera = kiss3d::camera::FirstPerson::new(Point3::new(0.0, 0.0, 0.0), Point3::new(-1.0, -1.0, -1.0));
-
-    camera.rebind_up_key   (Some(Key::W));
-    camera.rebind_down_key (Some(Key::S));
-    camera.rebind_left_key (Some(Key::A));
-    camera.rebind_right_key(Some(Key::D));
-
-    camera.rebind_rotate_button(Some(MouseButton::Button2));
-    camera.rebind_drag_button(None);
-
-    //window.render_with_camera()
-
-    while window.render_with_camera(&mut camera) {
-
-        //planet.set_local_translation(Translation3::<f32>::from(point_to_vector(&vec[index])));        
-                //vec[index]));
-        index += 1 as usize;
-        index = index % points_count;
-
-        //for i in 0..vec.len()-1 {
-        //    window.draw_line(&vec[i], &vec[i + 1], &Point3::<f32>::new(0.0, 1.0, 1.0));
-        //}
-        //window.draw_line(&vec[0], &vec[vec.len() - 1], &Point3::<f32>::new(0.0, 1.0, 1.0));
-
-        window.draw_line(&axis_0, &axis_x, &Point3::new(1.0, 0.0, 0.0));
-        window.draw_line(&axis_0, &axis_y, &Point3::new(0.0, 1.0, 0.0));
-        window.draw_line(&axis_0, &axis_z, &Point3::new(0.0, 0.0, 1.0));
-
-        sphere.prepend_to_local_rotation(&rot);
-        //planet.set_local_translation(Translation3::new(20.0 * angle.cos(), 20.0 * angle.sin(), 0.0));
-        //angle += PI / 180.0;
-    }
+    window.draw_line(&points[0], &points[points.len() - 1], &color); 
 }
