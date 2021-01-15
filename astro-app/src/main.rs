db3a8f0387315493aa78ff093d9acb25315cc0a0
Graph-Donte-Crypto <Dimasik841@gmail.com>
2021-01-15 21:22:53 +0300

    more fixes

--- astro-app/src/main.rs
+++ astro-app/src/main.rs
@@ -2,6 +2,7 @@ use astro_gui_lib as agl;
 use agl::kiss3d::nalgebra as na;
 use agl::kiss3d as kiss3d;
 
+
 use na::{Vector3, UnitQuaternion, Translation3, Point3};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
@@ -62,8 +63,7 @@ fn main() {
 
         //planet.set_local_translation(Translation3::<f32>::from(point_to_vector(&vec[index])));        
                 //vec[index]));
-
-        agl::draw_lines(&mut window, &points, &Point3::<f32>::new(1.0, 1.0, 1.0));
+        agl::draw_orbit(&mut window, &orbit, &Point3::<f32>::new(1.0, 1.0, 1.0));
 
         window.draw_line(&axis_0, &axis_x, &Point3::new(1.0, 0.0, 0.0));
         window.draw_line(&axis_0, &axis_y, &Point3::new(0.0, 1.0, 0.0));
