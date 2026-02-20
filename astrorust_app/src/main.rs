cbb4160c73d7a7a737ac254b5e8912ac6b232e16
Artur Sinila <personal@logarithmus.dev>
2026-02-20 11:03:20 +0100

    implement planetary flybys

--- astrorust_app/src/main.rs
+++ astrorust_app/src/main.rs
@@ -10,16 +10,17 @@ use astrorust_lib::orbit::flat::Orbit2DBuilder;
 use astrorust_lib::orbit::orbit_3d::{Orbit3D, Orbit3DBuilder};
 use astrorust_lib::state_vectors::StateVectors;
 use astrorust_lib::time::Time;
-use chrono::NaiveDate;
+use chrono::{NaiveDate, NaiveTime};
 use core::f64::consts::PI;
 use gui_lib::kiss3d::light::Light;
 use gui_lib::kiss3d::nalgebra as na;
 use gui_lib::kiss3d::window::Window;
 use na::{Point3, Translation3, Vector3};
+use std::f64::consts::TAU;
 use std::time::{Duration, Instant};
 
 const CAMERA_ACCELERATION: f64 = 0.001;
-const TIME_WARP: f64 = 1000_000.0;
+const TIME_WARP: f64 = 4000_000.0;
 // const TIME_WARP: f64 = 1.0;
 const STAR_RADIUS: f32 = 15.0;
 const PLANET_RADIUS: f32 = 7.0;
@@ -90,7 +91,10 @@ fn main() {
         &Vector3::from([0.0, 0.0, std::f32::consts::PI]),
     );
 
-    let epoch = NaiveDate::from_ymd_opt(2000, 1, 1).unwrap().and_time(Default::default()).and_utc();
+    let epoch = NaiveDate::from_ymd_opt(2000, 1, 1)
+        .unwrap()
+        .and_time(NaiveTime::from_hms_opt(12, 0, 0).unwrap())
+        .and_utc();
     let started_at_date = chrono::Utc::now();
     let started_at = Instant::now();
     while window.render_with_camera(&mut camera) {
@@ -102,26 +106,85 @@ fn main() {
             draw_orbit_and_current_position(&mut window, &eye, scale, planet, t);
         }
         let r = spacecraft.orbit.position(t);
-        if let Some(planet) = planets.iter().find(|planet| {
-            let Some(body) = &planet.body else {
-                return false;
-            };
-            (planet.orbit.position(t) - r).magnitude_squared()
-                < planet.orbit.orbit_2d.0.a() * (body.μ / planet.orbit.orbit_2d.0.mu()).powf(0.4)
-        }) {
+        if let Some((orbit, body)) = planets
+            .iter_mut()
+            .filter_map(|pl| Some((&mut pl.orbit, pl.body.as_ref()?)))
+            .find(|(orbit, body)| {
+                (orbit.position(t) - r).magnitude()
+                    < orbit.orbit_2d.0.a() * (body.μ / orbit.orbit_2d.0.mu()).powf(0.4)
+            })
+        {
             // Compute gravity assist
+            eprintln!("Spacecraft encounter with planet {}", body.name);
+            let (planet_r, planet_v) = orbit.position_and_velocity(t);
 
-            eprintln!("Spacecraft encounter with planet {}", planet.body.as_ref().unwrap().name);
-            let (planet_r, planet_v) = planet.orbit.position_and_velocity(t);
-            let r = r - planet_r;
-            let v = spacecraft.orbit.velocity(t) - planet_v;
+            // Convert heliocentric state vectors to planetocentric
+            let mut r = r - planet_r;
+            let mut v = spacecraft.orbit.velocity(t) - planet_v;
 
-            // rotate v by angle delta and r by (pi - delta)
+            // Unit vector from planet to periapsis of flyby hyperbola
+            let unit_e = (v.cross(&r.cross(&v)) / body.μ - r.normalize()).normalize();
 
-            let r = r + planet_r;
-            let v = v + planet_v;
+            dbg!("Flyby start: ", &r, &v);
 
-            // calculate new orbit from new r and v
+            // Reflect state vectors to get state vectors after the flyby
+            r = -r + 2.0 * r.dot(&unit_e) * unit_e;
+            let delta_v = 2.0 * v.dot(&unit_e) * unit_e;
+            v += delta_v;
+
+            dbg!("Flyby end: ", &r, &v, &delta_v, delta_v.magnitude());
+
+            r = (orbit.orbit_2d.0.a() * (body.μ / orbit.orbit_2d.0.mu()).powf(0.4)) * r.normalize();
+
+            // Convert planetocentric state vectors back to heliocentric
+            r += planet_r;
+            v += planet_v;
+
+            // Calculate new orbit from new state vectors
+            let h = r.cross(&v);
+            let r_mag = r.magnitude();
+            let a = r_mag * system.star.μ / (2.0 * system.star.μ - v.magnitude_squared() * r_mag);
+            let e = v.cross(&r.cross(&v)) / system.star.μ - r.normalize();
+            let i = (h[2] / h.magnitude()).acos();
+            let n = Vector3::z().cross(&h).normalize();
+            let Ω = if n[1] >= 0.0 { n[0].acos() } else { TAU - n[0].acos() };
+            let ω = if e[2] >= 0.0 {
+                (e.dot(&n) / (e.magnitude())).acos()
+            } else {
+                TAU - (e.dot(&n) / (e.magnitude())).acos()
+            };
+            let v_r = v.dot(&r.normalize());
+            let nu = if v_r >= 0.0 {
+                (e.dot(&r) / (e.magnitude() * r.magnitude())).acos()
+            } else {
+                TAU - (e.dot(&r) / (e.magnitude() * r.magnitude())).acos()
+            };
+            let e = e.magnitude();
+            let E = 2.0 * (((1.0 - e) / (1.0 + e)).sqrt() * (0.5 * nu).tan()).atan();
+            let M = E - e * E.sin();
+            let M0 = M - t.as_secs() * (system.star.μ / (a * a * a)).sqrt();
+
+            let new_orbit: EllipticOrbit = Orbit2DBuilder::default()
+                .std_grav_param(system.star.μ)
+                .semi_major_axis(a)
+                .eccentricity(e)
+                .mean_anomaly_at_t0(Angle::from_rad(M0).into())
+                .build()
+                .unwrap()
+                .into();
+            let new_orbit = Orbit3DBuilder::default()
+                .orbit_2d(new_orbit)
+                .inclination(i)
+                .long_of_asc_node(Ω)
+                .arg_of_periapsis(ω)
+                .build()
+                .unwrap();
+            dbg!(&spacecraft.orbit, &new_orbit);
+            spacecraft.orbit = new_orbit;
+            spacecraft.points = gui_lib::generate_orbit_points(&spacecraft.orbit, 100)
+                .iter()
+                .map(|point| point.map(|x| (scale * x) as f32))
+                .collect();
         }
         draw_orbit_and_current_position(&mut window, &eye, scale, &mut spacecraft, t);
 
